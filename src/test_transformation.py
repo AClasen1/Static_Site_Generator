4f3f5788a2aeb83234a62f75f1c94504b5664700
AClasen1 <aclasen.ac@gmail.com>
2024-10-28 11:56:41 -0500

    Added a function to split text nodes into multiple nodes based on a delimiter

--- src/test_transformation.py
+++ src/test_transformation.py
@@ -1,7 +1,7 @@
 import unittest
 from transformation import *
 
-class TestTransformation(unittest.TestCase):
+class TestTextToLeaf(unittest.TestCase):
     def test_text(self):
         textnode = TextNode("Default text", TextType.TEXT)
         text_node_as_html = text_node_to_html_node(textnode).to_html()
@@ -21,6 +21,34 @@ class TestTransformation(unittest.TestCase):
         textnode = TextNode("alt text", TextType.IMAGE, url = "imgurl.png")
         text_node_as_html = text_node_to_html_node(textnode).to_html()
         self.assertEqual(text_node_as_html, '<img src="imgurl.png" alt="alt text"></img>')
+
+class TestSplitDelimiter(unittest.TestCase):
+    def test_no_text(self):
+        styled_nodes = [TextNode("**bold text**", TextType.BOLD), TextNode("*italic text*", TextType.ITALIC)]
+        split_styled_nodes = split_nodes_delimiter(styled_nodes,"*",TextType.ITALIC)
+        for i in range(len(styled_nodes)):
+            self.assertEqual(styled_nodes[i],split_styled_nodes[i])
+
+    def test_no_matching_delimiters(self):
+        styled_nodes = [TextNode("**bold text**", TextType.TEXT), TextNode("*italic text*", TextType.TEXT)]
+        split_styled_nodes = split_nodes_delimiter(styled_nodes,"`",TextType.CODE)
+        for i in range(len(styled_nodes)):
+            self.assertEqual(styled_nodes[i],split_styled_nodes[i])
+
+    def test_basic_bold_split(self):
+        starter_node = [TextNode("Here's some **bold text** to split out", TextType.TEXT)]
+        split_node = split_nodes_delimiter(starter_node,"**",TextType.BOLD)
+        split_goal = [TextNode("Here's some ",TextType.TEXT), TextNode("bold text", TextType.BOLD), TextNode(" to split out", TextType.TEXT)]
+        for i in range(len(split_node)):
+            self.assertEqual(split_node[i], split_goal[i])
+
+    def test_bold_italic_split(self):
+        starter_node = [TextNode("Here's some **bold text** to split out, and then some *italic text*", TextType.TEXT)]
+        bold_split_node = split_nodes_delimiter(starter_node,"**",TextType.BOLD)
+        italic_split_node = split_nodes_delimiter(bold_split_node,"*",TextType.ITALIC)
+        split_goal = [TextNode("Here's some ",TextType.TEXT), TextNode("bold text", TextType.BOLD), TextNode(" to split out, and then some ", TextType.TEXT), TextNode("italic text", TextType.ITALIC)]
+        for i in range(len(italic_split_node)):
+            self.assertEqual(italic_split_node[i], split_goal[i])
 
 if __name__ == "__main__":
     unittest.main()
--- src/transformation.py
+++ src/transformation.py
@@ -17,3 +17,20 @@ def text_node_to_html_node(text_node):
             return LeafNode(value = "", tag = "img", props = {"src": text_node.url, "alt": text_node.text})
         case _:
             raise TypeError("Inapplicable type supplied")
+        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    split_nodes = []
+    for node in old_nodes:
+        if node.text_type != "text":
+            split_nodes.append(node)
+        elif node.text.count(delimiter) % 2 != 0:
+            raise ValueError("Odd number of delimiters in text node to be split")
+        else:
+            split_node = node.text.split(delimiter)
+            for i in range(len(split_node)):
+                if len(split_node[i]) != 0:
+                    new_node_type = TextType.TEXT
+                    if i % 2 == 1:
+                        new_node_type = text_type
+                    split_nodes.append(TextNode(split_node[i], new_node_type))
+    return split_nodes
