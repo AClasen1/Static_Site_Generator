14b2d4f252935ef56b5a361a2aeab2f55ddd4edd
AClasen1 <aclasen.ac@gmail.com>
2024-11-05 23:33:18 -0600

    added a function to catagorize markdown blocks

--- src/block_transformation.py
+++ src/block_transformation.py
@@ -5,5 +5,34 @@ def markdown_to_blocks(markdown):
     markdown_blocks = markdown.split("\n\n")
     for block in markdown_blocks:
         if block != "":
-            whitespace_stripped_markdown_blocks.append(block.strip(" \n"))
+            whitespace_stripped_markdown_blocks.append(block.strip(" \n\t"))
     return whitespace_stripped_markdown_blocks
+
+def block_to_block_type(markdown_block):
+    block_type = "paragraph"
+    markdown_block_lines = markdown_block.split("\n")
+    unordered_list_lines = 0
+    ordered_list_lines = 0
+    quote_lines = 0
+    if markdown_block.startswith("```") and markdown_block.endswith("```"):
+        block_type = "code"
+    elif (len(markdown_block.lstrip("#")) < len(markdown_block)
+          and len(markdown_block.lstrip("#")) >= len(markdown_block) - 6
+          and markdown_block.lstrip("#").startswith(" ")):
+        block_type = "heading"
+    else: 
+        for i in range(len(markdown_block_lines)):
+            line_number = i + 1
+            if markdown_block_lines[i].startswith(">"):
+                quote_lines += 1
+            if markdown_block_lines[i].startswith("* ") or markdown_block_lines[i].startswith("- "):
+                unordered_list_lines += 1
+            if markdown_block_lines[i].startswith(f"{line_number}. "):
+                ordered_list_lines += 1
+        if len(markdown_block_lines) == unordered_list_lines:
+            block_type = "unordered_list"
+        elif len(markdown_block_lines) == ordered_list_lines:
+            block_type = "ordered_list"
+        elif len(markdown_block_lines) == quote_lines:
+            block_type = "quote"
+    return block_type   
--- src/test_blocktransformation.py
+++ src/test_blocktransformation.py
@@ -29,6 +29,96 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         goal_blocks = ['# This is a heading', 'This is a paragraph of text. It has some **bold** and *italic* words inside of it.', '* This is the first list item in a list block\n* This is a list item\n* This is another list item']
         self.assertListEqual (markdown_to_blocks(text_to_blockify), goal_blocks)    
 
+class TestBlockToBlockType(unittest.TestCase):
+    def test_one_of_each(self):
+        text_to_blockify = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+
+1. One
+2. Two
+3. Three
+4. Four
+5. Five
+
+```SELECT COUNT(*) 
+FROM rpt.AnnualMemberStats
+WHERE Year = 2024```
+
+>>>Who are you quoting?"""
+
+        text_blocks = markdown_to_blocks(text_to_blockify)
+        block_types = list(map(block_to_block_type, text_blocks))
+        self.assertListEqual(block_types, ["heading", "paragraph", "unordered_list", "ordered_list", "code", "quote"])
+
+    def test_headers(self):
+        text_to_blockify = """# This is a heading
+
+## This is a heading        
+
+### This is a heading
+
+#### This is a heading        
+
+##### This is a heading        
+
+###### This is a heading
+
+#This is not
+
+####### And neither is this
+"""
+
+        text_blocks = markdown_to_blocks(text_to_blockify)
+        block_types = list(map(block_to_block_type, text_blocks))
+        self.assertListEqual(block_types, ["heading", "heading", "heading", "heading", "heading", "heading", "paragraph", "paragraph"])
+
+    def test_lists_and_quotes(self):
+        text_to_blockify = """- One line unordered list
+
+1. One line ordered list
+
+* Mixed bullet
+- Unordered list
+* Still works
+
+1. This
+2. Is
+3. An
+4. Ordered
+5. List
+
+0. But
+1. This
+2. Is
+3. Not
+
+- And
+-This
+- Is
+- Not
+-An
+- Unordered
+- List
+
+-And neither is this
+
+>This is a quote
+
+>These are
+>quotes
+
+>But
+This
+>isn't"""
+
+        text_blocks = markdown_to_blocks(text_to_blockify)
+        block_types = list(map(block_to_block_type, text_blocks))
+        self.assertListEqual(block_types, ["unordered_list", "ordered_list", "unordered_list", "ordered_list", "paragraph", "paragraph", "paragraph", "quote", "quote", "paragraph"])
 
 if __name__ == "__main__":
     unittest.main()
