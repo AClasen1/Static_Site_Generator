209876610eaf64bbdaf3480f6ed616e7d9d6fc41
AClasen1 <aclasen.ac@gmail.com>
2024-11-10 17:55:12 -0600

    cleaning out Main

--- src/main.py
+++ src/main.py
@@ -3,19 +3,7 @@ from transformation import *
 from block_transformation import *
 
 def main():
-	text_to_blockify = """# This is a heading
-
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
-
-
-
-
-
-
-
-* This is the first list item in a list block
-* This is a list item
-* This is another list item"""
-	print(markdown_to_blocks(text_to_blockify))
+	pass
 
 main()
+
